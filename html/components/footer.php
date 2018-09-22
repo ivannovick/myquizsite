5364d876c0c333e8d5a5e9ddd69ceaea29a9fa65
Ivan Novick <ivannovick@users.noreply.github.com>
2018-09-21 20:36:38 -0700

    Update footer.php

--- html/components/footer.php
+++ html/components/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer">
- <div class="reportbug">We are open source now: <a href="https://github.com/ivannovick/myquizsite">Find Us on GitHub</a>"/></div>
+ <div class="reportbug">We are open source now: <a href="https://github.com/ivannovick/myquizsite">Find Us on GitHub</a></div>
  <div class="lgl" style='clear:both'>&copy; 2007-2018, My <?= $language->get_normalized_name() ?> Quiz, All rights reserved.</div>
 </footer>
